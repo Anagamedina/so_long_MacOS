0b7815be5896a6c9d494e43fc5eb910e5703ae3b
Anagamedina <anaga.medina@gmail.com>
2024-08-11 19:48:26 +0200

    itemsinprocess

---
+++ src/map_items.c
@@ -0,0 +1,13 @@
+//funcion que sirve para empezar en rectangulo pequeño 
+//funcion para validar 1 0 P E C (ITEMS)
+
+void    map_items(t_map *copy_map)
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    while 
+
+}
