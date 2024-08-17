abc3ea8a87ee763f1f84a3e9405514182cbaaf73
Anagamedina <anaga.medina@gmail.com>
2024-08-17 22:10:52 +0200

    ventananegra

--- src/main.c
+++ src/main.c
@@ -20,10 +20,9 @@ int mlx_string_put(void *mlx_ptr, void *win_ptr, int x, int y, int color, char *
 void my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
     char *dst;
-
-    if (data == NULL || data->addr == NULL)
+    if (x < 0 || x >= 1920 || y < 0 || y >= 1080)
     {
-        printf("Error: data or data->addr is NULL.\n");
+        printf("Pixel out of bounds: (%d, %d)\n", x, y);
         return;
     }
 
@@ -31,9 +30,24 @@ void my_mlx_pixel_put(t_data *data, int x, int y, int color)
     *(unsigned int*)dst = color;
 }
 
+
+static void free_resources(void *mlx_ptr, void *win_ptr, t_data *image)
+{
+    if (image && image->img)
+        mlx_destroy_image(mlx_ptr, image->img);
+    if (win_ptr)
+        mlx_destroy_window(mlx_ptr, win_ptr);
+    if (mlx_ptr)
+    {
+        mlx_destroy_display(mlx_ptr);
+        free(mlx_ptr);
+    }
+}
+
 int main(int argc, char **argv)
 {
     t_game  *game;
+    t_data  image;
     int     ccoins;
     void    *mlx_ptr;
     void    *win_ptr;
@@ -70,102 +84,41 @@ int main(int argc, char **argv)
     {
         printf("Error creating window.\n");
         mlx_destroy_display(mlx_ptr);
-        free(mlx_ptr);
+        free_resources(mlx_ptr, NULL, NULL); 
         return (1);
     }
 
-    // Asignar imagen a `game->image`
-    // game->image->img = mlx_new_image(mlx_ptr, 1920, 1080);
-    // if (game->image->img == NULL)
-    // {
-    //     printf("Error creating image.\n");
-    //     free(mlx_ptr);
-    //     return (1);
-    // }
+    image.img = mlx_new_image(mlx_ptr, 1920, 1080);
+    if (image.img == NULL)
+    {
+        printf("Error creating image.\n");
+        free_resources(mlx_ptr, NULL, NULL);
+        return (1);
+    }
 
-    game->image->addr = mlx_get_data_addr(game->image->img, &game->image->bits_per_pixel, &game->image->line_length, &game->image->endian);
-    if (game->image->addr == NULL)
+    image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel, &image.line_length, &image.endian);
+    if (image.addr == NULL)
     {
         printf("Error getting image data address.\n");
-        free(mlx_ptr);
+        free_resources(mlx_ptr, win_ptr, &image);
         return (1);
     }
 
-   // Renderizar texto en la ventana
-    mlx_string_put(mlx_ptr, win_ptr, 50, 50, 0x00FF00, "Hello, MiniLibX!");
+    // Imprimir los valores para depuración
+    printf("Bits per pixel: %d\n", image.bits_per_pixel);
+    printf("Line length: %d\n", image.line_length);
+    printf("Endian: %d\n", image.endian);
+    // Dibujar un pixel rojo en la imagen
+    my_mlx_pixel_put(&image, 960, 540, 0x00FF0000); // Color rojo
 
-    // Dibujar un pixel en la imagen
-    // my_mlx_pixel_put(game->image, 5, 5, 0x00FF0000);
-
-    // Mostrar la imagen en la ventana
-    // mlx_put_image_to_window(mlx_ptr, win_ptr, game->image->img, 0, 0);
+    // Poner la imagen en la ventana
+    mlx_put_image_to_window(mlx_ptr, win_ptr, image.img, 0, 0);
 
     // Entrar en el loop de eventos de MiniLibX
     mlx_loop(mlx_ptr);
 
-    // Liberar recursos (esto debería estar al final del programa)
-    mlx_destroy_window(mlx_ptr, win_ptr);
-    mlx_destroy_display(mlx_ptr);
-    free(mlx_ptr);
+    // Liberar recursos
+    free_resources(mlx_ptr, win_ptr, &image);
 
     return (0);
 }
-
-/*int	main(int argc, char **argv)
-{
-	t_game	*game;
-    int		ccoins;
-	void	*mlx_ptr;
-	void	*win_ptr;
-    t_data  *img;
-
-	game = NULL;
-    img = NULL;
-	ccoins = 0;
-	if (argc != 2)
-		handle_exit(ERROR_INVALID_ARG, 28);
-	else
-	{
-		game = init_game();
-		if (game != NULL)
-		{
-			map_format_border_check(argv[1], game->map);
-			map_items(game->map);
-			validation_player(&ccoins, game->map);
-		}
-		free_game(game);
-	}
-    mlx_ptr = mlx_init();
-    if (mlx_ptr == NULL)
-    {
-        printf("Error initializing mlx.\n");
-        free(mlx_ptr);
-        return (1);
-    }
-    win_ptr = mlx_new_window(mlx_ptr, 1920, 1080, "Hello Ana! so_long");
-    if (win_ptr == NULL)
-    {
-        printf("Error creating window.\n");
-        return (free(mlx_ptr), 1);
-    }
-    img->img = mlx_new_image(mlx_ptr, 1920, 1080);
-    if (img->img == NULL)
-    {
-        printf("Error creating image.\n");
-        return (free(mlx_ptr), 1);
-    }
-    img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length, &img->endian);
-    if (img->addr == NULL)
-    {
-        printf("Error creating image.\n");
-        return (free(mlx_ptr), 1);
-    }
-    my_mlx_pixel_put(img, 5, 5, 0x00FF0000);
-    mlx_put_image_to_window(mlx_ptr, win_ptr, img->img, 0, 0);
-
-    //mlx_destroy_window(mlx_ptr, win_ptr);
-    //mlx_destroy_display(mlx_ptr);
-    //free(mlx_ptr);
-    //return (0);
-    mlx_loop(mlx_ptr);
-}*/
