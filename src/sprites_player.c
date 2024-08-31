b9399df0aea08f56568c63c4ae1768fba4cede41
Anagamedina <anaga.medina@gmail.com>
2024-09-01 00:11:55 +0200

    modify move_player

--- src/sprites_player.c
+++ src/sprites_player.c
@@ -6,7 +6,7 @@
 /*   By: anamedin <anamedin@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 16:26:04 by anamedin          #+#    #+#             */
-/*   Updated: 2024/08/31 23:16:52 by anamedin         ###   ########.fr       */
+/*   Updated: 2024/09/01 00:10:44 by anamedin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,36 +52,37 @@ void	move_player(t_game *game, int new_x, int new_y, int player_sprite)
 	}
 
 	// Verifica si el movimiento es hacia una posición válida
-	if (game->map->matrix[new_y][new_x] == WALL)
+	if (game->map->matrix[new_x][new_y] == WALL)
 		return; // No hacer nada si es una pared
 
 	game->player_sprite = player_sprite;
 
 	// Verifica si el jugador alcanza la salida
-	if (game->map->matrix[new_y][new_x] == EXIT && game->map->coins == 0)
+	if (game->map->matrix[new_x][new_y] == EXIT && game->map->coins == 0)
 	{
 		ft_victory(game);
 		return;
 	}
 
-	// Si el jugador se mueve a un piso o recoge una moneda
-	if (game->map->matrix[new_y][new_x] == FLOOR || game->map->matrix[new_y][new_x] == COINS)
+	// Si el jugador se mueve a un suelo o recoge una moneda
+	if (game->map->matrix[new_x][new_y] == FLOOR || game->map->matrix[new_x][new_y] == COINS)
 	{
 		// Actualiza la posición anterior del jugador a FLOOR
-		game->map->matrix[last_y][last_x] = FLOOR;
+		game->map->matrix[last_x][last_y] = FLOOR;
 
 		// Si recoge una moneda, decrementa el contador
-		if (game->map->matrix[new_y][new_x] == COINS)
+		if (game->map->matrix[new_x][new_y] == COINS)
 			game->map->coins--;
-
+		printf("coins: %d\n", game->map->coins);
 		// Actualiza la nueva posición del jugador
 		game->map->player_pos.x = new_x;
 		game->map->player_pos.y = new_y;
-		game->map->matrix[new_y][new_x] = PLAYER;
+		game->map->matrix[new_x][new_y] = PLAYER;
 		printf("Moving player to: new_x=%d, new_y=%d\n", new_x, new_y);
 
 		// Incrementa el contador de movimientos
 		game->movements++;
+		printf("movimientos: %d\n", game->movements);
 
 		// Renderiza el mapa actualizado
 		identify_images(game);
@@ -93,13 +94,15 @@ int	handle_input(int keysym, t_game *game)
 {
 	if (keysym == KEY_W)
 		move_player(game, game->map->player_pos.y - 1, game->map->player_pos.x, BACK);
-	if (keysym == KEY_A)
+	/*else if (keysym == KEY_A)
 		move_player(game, game->map->player_pos.y, game->map->player_pos.x - 1, LEFT);
-	if (keysym == KEY_S)
+	*/
+	else if (keysym == KEY_S)
 		move_player(game, game->map->player_pos.y + 1, game->map->player_pos.x, FRONT);
-	if (keysym == KEY_D)
+	/*else if (keysym == KEY_D)
 		move_player(game, game->map->player_pos.y, game->map->player_pos.x + 1, RIGHT);
-	if (keysym == KEY_ESC)
+	*/
+	else if (keysym == KEY_ESC)
 		ft_close_game(game);
 	return (0);
 }
