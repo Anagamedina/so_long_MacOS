5864efa4ebd29dd4611559f649ad374ba337c121
Anagamedina <anaga.medina@gmail.com>
2024-09-02 18:36:26 +0200

    map_init norminette Ok

--- src/map_init.c
+++ src/map_init.c
@@ -6,7 +6,7 @@
 /*   By: anamedin <anamedin@student.42barcelona.c>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 21:36:41 by anamedin          #+#    #+#             */
-/*   Updated: 2024/09/02 14:12:45 by anamedin         ###   ########.fr       */
+/*   Updated: 2024/09/02 18:33:27 by anamedin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,17 @@ static int	ft_open_map(char *path)
 	return (fd);
 }
 
-static char	*read_file(char *path)
+static char	*read_file(char *path, int i)
 {
 	int		fd;
 	char	*map1d;
 	char	buffer;
-	int		i;
 	ssize_t	bytes_read;
 
 	fd = ft_open_map(path);
-	i = 0;
 	bytes_read = read(fd, &buffer, 1);
-	while(bytes_read > 0)
-		i++;
-	//close (fd);
+	while (bytes_read > 0 && ++i)
+		bytes_read = read(fd, &buffer, 1);
 	if (i == 0)
 		handle_exit(ERROR_OPEN_FILE, 26);
 	map1d = (char *)malloc(i + 1);
@@ -43,7 +40,7 @@ static char	*read_file(char *path)
 		handle_exit(ERROR_MEMORY, 35);
 	fd = open(path, O_RDONLY);
 	if (fd < 0)
-        handle_exit("Error reopening file.\n", 22);
+		handle_exit("Error reopening file.\n", 22);
 	bytes_read = read(fd, map1d, i);
 	if (bytes_read < 0)
 		handle_exit(ERROR_OPEN_FILE, 26);
@@ -59,7 +56,7 @@ void	read_map(char *path, t_map *map)
 	int		i;
 	int		line;
 
-	map1d = read_file(path);
+	map1d = read_file(path, 0);
 	if (map1d == NULL)
 		return ;
 	map->matrix = ft_split(map1d, '\n');
